6417a8a2e4aeeceba94dc50d6c0fba26c1d0a8ef
Matt Burgess <matt@smallbatch.io>
2020-10-15 16:41:34 +0700

    remove queries file

--- src/components/queries.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { getInstanceIdentifier } from '../utils/ethUtils';
-const identifier = getInstanceIdentifier();
-
-// export const getScheme = async () => {
-//   const { data } = await axios.get(`/schemes/${identifier}`);
-//   return data.scheme;
-// };
