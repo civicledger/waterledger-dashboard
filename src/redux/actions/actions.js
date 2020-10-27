7c8864bf8c648657a5c82842bfed983ffd9fb27d
Dagan Herceg <dher3698@uni.sydney.edu.au>
2020-10-27 16:49:57 +1100

    reverted notification timeout

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -42,7 +42,7 @@ export const addNotification = payload => {
     dispatch(createNotification(payload));
     setTimeout(() => {
       dispatch(removeNotification(payload.id));
-    }, 10000);
+    }, 100000000);
   };
 };
 
