d7b748124992faccff981d2fc85b2229efe653f3
Dagan Herceg <dher3698@uni.sydney.edu.au>
2020-10-27 14:28:22 +1100

    fixed conflict

--- src/components/history/TradesList.js
+++ src/components/history/TradesList.js
@@ -9,24 +9,16 @@ export default ({ trades, isLoading = false, limit = null }) => {
   }
 
   return (
-  <div className="p-0 pt-3 lg:p-5 mt-3 rounded bg-steel-800 lg:p-5">
-    <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Recent Trades</h2>
-    <table className="table relative w-full transaction-list ">
-      <thead>
-        <tr className="text-leftfont-semibold bg-steel-700">
-          <th className="p-2">Price / ML</th>
-          <th className="p-2">Volume</th>
-          <th className="p-2 lg:table-cell">From Zone</th>
-          <th className="p-2 lg:table-cell">To Zone</th>
-          <th className="xl:table-cell p-2">Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {trades.length === 0 && (
-          <tr>
-            <td colSpan="5" className="p-5 pb-0 text-center">
-              No Trades have been recorded
-            </td>
+    <div className="p-0 pt-3 lg:p-5 mt-3 rounded bg-steel-800 lg:p-5">
+      <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Recent Trades</h2>
+      <table className="table relative w-full transaction-list ">
+        <thead>
+          <tr className="text-left font-semibold bg-steel-700">
+            <th className="p-2">Price / ML</th>
+            <th className="p-2">Volume</th>
+            <th className="p-2 lg:table-cell">From Zone</th>
+            <th className="p-2 lg:table-cell">To Zone</th>
+            <th className="xl:table-cell p-2">Date</th>
           </tr>
         </thead>
         <tbody>
