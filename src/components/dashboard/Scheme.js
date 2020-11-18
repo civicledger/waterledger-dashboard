fe69f4ede69f02cce72bd4940856d515c7ffbf38
Matt Burgess <matt@smallbatch.io>
2020-11-18 13:10:03 +0700

    change trade price to traded price

--- src/components/dashboard/Scheme.js
+++ src/components/dashboard/Scheme.js
@@ -16,7 +16,7 @@ export default props => {
     keepPreviousData: true,
   });
 
-  if (scheme === undefined) scheme = { lastTradePrice: 0, name: "", orderbookDeployment: { address: "" } };
+  if (scheme === undefined) scheme = { lastTradedPrice: 0, name: "", orderbookDeployment: { address: "" } };
 
   return (
     <div className="flex-auto w-full flex-wrap lg:w-1/4 p-0 lg:p-5 steel-gradient mr-0 lg:mr-2 text-center flex flex-row lg:flex-col scheme-panel">
@@ -27,7 +27,7 @@ export default props => {
       <div className="w-2/3 lg:w-full lg:flex-grow lg:mt-5">
         <div className="my-4 text-lg lg:text-xl scheme-name mt-0">{scheme.name}</div>
         Last Traded Price
-        <div className="txt-xl lg:text-4xl">{formatAmount(scheme.lastTradePrice ?? 0)}</div>
+        <div className="txt-xl lg:text-4xl">{formatAmount(scheme.lastTradedPrice ?? 0)}</div>
         <div className="mb-3 text-xs text-steel-300 hidden lg:block">
           <span className="inline-block py-1 px-2 border rounded border-steel-300">
             <i className="text-xs mr-2 fab fa-ethereum"></i>
