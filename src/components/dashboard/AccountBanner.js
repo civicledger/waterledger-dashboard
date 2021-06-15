1b44026d83e1b8241216982daf35f125c810f634
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-16 09:15:17 +1000

    Changing button and accountBanner

--- src/components/dashboard/AccountBanner.js
+++ src/components/dashboard/AccountBanner.js
@@ -6,21 +6,19 @@ export default () => {
     <div className="bg-steel-700 mb-10 rounded p-5">
       <h2 className="ml-2 pb-6">You are not logged in. Please login or signup to start using Water Ledger dashboard.</h2>
       <div className="text-right col-span-3">
-        <Link to="/login">
-          <button
-            type="submit"
-            className={`justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-sm text-steel-white bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-          >
-            <i className="fal fa-sign-in mr-2"></i>Log In
-          </button>
+        <Link
+          to="/login"
+          type="submit"
+          className="py-2 px-4 border border-transparent text-sm font-medium rounded-sm text-steel-white bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          <i className="fal fa-sign-in mr-2"></i>Log In
         </Link>
-        <Link to="/signup">
-          <button
-            type="submit"
-            className={`justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-sm text-steel-white bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-4`}
-          >
-            <i className="fal fa-user-plus fa-fw mr-2"></i>Sign Up
-          </button>
+        <Link
+          to="/signup"
+          type="submit"
+          className="py-2 px-4 border border-transparent text-sm font-medium rounded-sm text-steel-white bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-4"
+        >
+          <i className="fal fa-user-plus fa-fw mr-2"></i>Sign Up
         </Link>
       </div>
     </div>
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -31,38 +31,36 @@ export default props => {
   return (
     <div className="py-5 px-5 lg:px-10 flex-grow pb-5">
       {!loggedIn && <AccountBanner />}
-      {loggedIn && (
-        <div className="flex flex-col lg:flex-row w-full">
-          <Scheme />
+      <div className="flex flex-col lg:flex-row w-full">
+        <Scheme />
 
-          <div className="flex-auto w-full lg:w-3/4">
-            <Graph />
+        <div className="flex-auto w-full lg:w-3/4">
+          <Graph />
 
-            <h2 className="lg:ml-2 pb-3 lg:pb-6 text-2xl">Order Book</h2>
+          <h2 className="lg:ml-2 pb-3 lg:pb-6 text-2xl">Order Book</h2>
 
-            <div className="flex">
-              <div className="w-full">
-                <div className="flex flex-col xl:flex-row ">
-                  <div className="flex-1 p-0 pt-3 lg:p-5 rounded bg-steel-800">
-                    <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Bids</h2>
+          <div className="flex">
+            <div className="w-full">
+              <div className="flex flex-col xl:flex-row ">
+                <div className="flex-1 p-0 pt-3 lg:p-5 rounded bg-steel-800">
+                  <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Bids</h2>
 
-                    <OrderList orders={buyOrders} type="buy" deleteOrder={deleteOrder} isLoading={buysLoading} waterAccounts={waterAccounts} />
-                    {loggedIn && <OrderButton type="buy" openOrderForm={() => dispatch(openOrderForm({ type: "buy", price: "", quantity: "" }))} />}
-                  </div>
+                  <OrderList orders={buyOrders} type="buy" deleteOrder={deleteOrder} isLoading={buysLoading} waterAccounts={waterAccounts} />
+                  {loggedIn && <OrderButton type="buy" openOrderForm={() => dispatch(openOrderForm({ type: "buy", price: "", quantity: "" }))} />}
+                </div>
 
-                  <div className="rounded mr-0 lg:mr-1 flex-1 p-0 pt-3 lg:p-5 mt-3 xl:ml-1 xl:mt-0 bg-steel-800">
-                    <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Offers</h2>
+                <div className="rounded mr-0 lg:mr-1 flex-1 p-0 pt-3 lg:p-5 mt-3 xl:ml-1 xl:mt-0 bg-steel-800">
+                  <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Offers</h2>
 
-                    <OrderList orders={sellOrders} type="sell" deleteOrder={deleteOrder} isLoading={sellsLoading} waterAccounts={waterAccounts} />
-                    {loggedIn && <OrderButton type="sell" openOrderForm={() => dispatch(openOrderForm({ type: "sell", price: "", quantity: "" }))} />}
-                  </div>
+                  <OrderList orders={sellOrders} type="sell" deleteOrder={deleteOrder} isLoading={sellsLoading} waterAccounts={waterAccounts} />
+                  {loggedIn && <OrderButton type="sell" openOrderForm={() => dispatch(openOrderForm({ type: "sell", price: "", quantity: "" }))} />}
                 </div>
               </div>
             </div>
           </div>
         </div>
-      )}
-      {loggedIn && <TradesList trades={trades} isLoading={tradesLoading} limit="10" />}
+      </div>
+      <TradesList trades={trades} isLoading={tradesLoading} limit="10" />
     </div>
   );
 };
