322b80cab54a3ec3f6dea9e9fd5d74736e3bd7e6
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-15 13:00:26 +1000

    Frontend changes for dashboard order deletion

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -19,6 +19,11 @@ export default class BaseService {
     return await axios.get(`${this.entity}/${id}`, { headers });
   }
 
+  async delete(id) {
+    const headers = this.getHeaders();
+    return await axios.delete(`${this.entity}/${id}`, { headers });
+  }
+
   getHeaders() {
     const token = localStorage.getItem("token");
     return { Authorization: `Bearer ${token}`, "x-scheme": getInstanceIdentifier() };
