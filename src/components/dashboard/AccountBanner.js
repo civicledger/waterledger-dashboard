878e88e3bac991bfe43cfa889382e329758e4de9
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-16 12:39:15 +1000

    Debugging Safari browser

--- src/components/dashboard/AccountBanner.js
+++ src/components/dashboard/AccountBanner.js
@@ -6,19 +6,21 @@ export default () => {
     <div className="bg-steel-700 mb-10 rounded p-5">
       <h2 className="ml-2 pb-6">You are not logged in. Please login or sign up to start using Water Ledger dashboard.</h2>
       <div className="text-right">
-        <Link
-          to="/login"
-          type="submit"
-          className="py-2 px-4 font-medium rounded-sm text-steel-white bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          <i className="fal fa-sign-in mr-2"></i>Log In
+        <Link to="/login">
+          <button
+            type="submit"
+            className="py-2 px-4 font-medium rounded-sm bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500"
+          >
+            <i className="fal fa-sign-in mr-2"></i>Log In
+          </button>
         </Link>
-        <Link
-          to="/signup"
-          type="submit"
-          className="py-2 px-4 font-medium rounded-sm text-steel-white bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-4"
-        >
-          <i className="fal fa-user-plus mr-2"></i>Sign Up
+        <Link to="/signup">
+          <button
+            type="submit"
+            className="py-2 px-4 font-medium rounded-sm bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 ml-4"
+          >
+            <i className="fal fa-user-plus mr-2"></i>Sign Up
+          </button>
         </Link>
       </div>
     </div>
