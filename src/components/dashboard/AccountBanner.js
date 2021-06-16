9fce52ca819c784feb3fa17b4ba39a87eda91281
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-16 16:22:05 +1000

    Changed link color

--- src/components/dashboard/AccountBanner.js
+++ src/components/dashboard/AccountBanner.js
@@ -6,12 +6,12 @@ export default () => {
     <div className="bg-steel-700 mb-8 rounded p-5 grid grid-cols-3">
       <h2 className="ml-5 col-span-2">
         You are not logged in. Please
-        <Link to="/login" className="text-gray-500 hover:text-indigo-500">
+        <Link to="/login" className="text-gray-500 hover:text-gray-600">
           {" "}
           login{" "}
         </Link>
         or
-        <Link to="/signup" className="text-gray-500 hover:text-indigo-500">
+        <Link to="/signup" className="text-gray-500 hover:text-gray-600">
           {" "}
           sign up{" "}
         </Link>
