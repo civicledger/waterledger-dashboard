800e39a2491593d0a4d200141f107dccfb0a1e29
Dagan Herceg <dagan.herceg@gmail.com>
2021-06-16 15:52:57 +1000

    changed approach to remove nested grids

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import TradesList from "../history/TradesList";
 import OrderList from "../orders/OrderList";
 import AccountBanner from "./AccountBanner";
 import Scheme from "./Scheme";
+import WaterAccountsList from "./WaterAccountsList";
 
 import Graph from "./Graph";
 
@@ -29,36 +30,41 @@ export default props => {
   const waterAccounts = licence ? licence.accounts : [];
 
   return (
-    <div className="grid grid-cols-12 py-5 px-5 lg:px-10 pb-5">
-      <div className="col-span-12">
+    <div className="grid grid-cols-8 py-5 px-5 lg:px-10 pb-5">
+      <div className="col-span-8">
         <AccountBanner />
       </div>
-      <div className="lg:flex col-span-12 lg:col-span-4 xl:col-span-3">
+      <div className="p-5 mr-0 mr-2 row-span-4 col-span-8 lg:col-span-2 flex flex-col justify-between">
         <Scheme />
-      </div>
-      <div className="col-span-12 lg:col-span-8 xl:col-span-9">
-        <Graph />
-        <div className="col-span-12 lg:col-span-9 lg:col-start-4">
-          <h2 className="lg:ml-2 pb-3 lg:pb-6 text-2xl">Order Book</h2>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="col-span-2 lg:col-span-1 p-0 pt-3 lg:p-5 rounded bg-steel-800">
-              <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Bids</h2>
-
-              <OrderList orders={buyOrders} type="buy" deleteOrder={deleteOrder} isLoading={buysLoading} waterAccounts={waterAccounts} />
-              {loggedIn && <OrderButton type="buy" openOrderForm={() => dispatch(openOrderForm({ type: "buy", price: "", quantity: "" }))} />}
-            </div>
-
-            <div className="col-span-2 lg:col-span-1 p-0 pt-3 lg:p-5 rounded bg-steel-800">
-              <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Offers</h2>
-
-              <OrderList orders={sellOrders} type="sell" deleteOrder={deleteOrder} isLoading={sellsLoading} waterAccounts={waterAccounts} />
-              {loggedIn && <OrderButton type="sell" openOrderForm={() => dispatch(openOrderForm({ type: "sell", price: "", quantity: "" }))} />}
-            </div>
-          </div>
+        <div>
+          <WaterAccountsList />
         </div>
       </div>
-      <div className="col-span-12 ">
-        <TradesList className="col-span-12" trades={trades} isLoading={tradesLoading} limit="10" />
+      <div className="col-span-8 lg:col-span-6">
+        <Graph />
+      </div>
+      <div className="col-span-8 lg:col-span-6">
+        <h2 className="lg:ml-2 pb-3 lg:pb-6 text-2xl">Order Book</h2>
+      </div>
+      <div className="col-span-8 lg:col-span-3">
+        <div className="p-0 pt-3 lg:p-5 rounded bg-steel-800">
+          <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Bids</h2>
+
+          <OrderList orders={buyOrders} type="buy" deleteOrder={deleteOrder} isLoading={buysLoading} waterAccounts={waterAccounts} />
+          {loggedIn && <OrderButton type="buy" openOrderForm={() => dispatch(openOrderForm({ type: "buy", price: "", quantity: "" }))} />}
+        </div>
+      </div>
+
+      <div className="col-span-8 lg:col-span-3">
+        <div className="p-0 pt-3 lg:p-5 rounded bg-steel-800">
+          <h2 className="text-xl mb-3 ml-5 lg:ml-0 font-semibold">Offers</h2>
+
+          <OrderList orders={sellOrders} type="sell" deleteOrder={deleteOrder} isLoading={sellsLoading} waterAccounts={waterAccounts} />
+          {loggedIn && <OrderButton type="sell" openOrderForm={() => dispatch(openOrderForm({ type: "sell", price: "", quantity: "" }))} />}
+        </div>
+      </div>
+      <div className="col-span-8 ">
+        <TradesList trades={trades} isLoading={tradesLoading} limit="10" />
       </div>
     </div>
   );
