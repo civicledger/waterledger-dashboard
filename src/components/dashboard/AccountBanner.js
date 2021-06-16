2a5d86dd7ea1b4a9086953928e8ec98cb67c1fac
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-16 16:14:10 +1000

    Changed link color to darker white

--- src/components/dashboard/AccountBanner.js
+++ src/components/dashboard/AccountBanner.js
@@ -6,18 +6,18 @@ export default () => {
     <div className="bg-steel-700 mb-8 rounded p-5 grid grid-cols-3">
       <h2 className="ml-5 col-span-2">
         You are not logged in. Please
-        <Link to="/login" className="text-steel-300 hover:text-indigo-300">
+        <Link to="/login" className="text-gray-500 hover:text-indigo-500">
           {" "}
           login{" "}
         </Link>
         or
-        <Link to="/signup" className="text-steel-300 hover:text-indigo-500">
+        <Link to="/signup" className="text-gray-500 hover:text-indigo-500">
           {" "}
           sign up{" "}
         </Link>
         to start using Water Ledger dashboard.
       </h2>
-      <div className="text-right col-span-1">
+      <div className="text-right">
         <Link to="/login" className="mr-3 p-2 px-8 border-steel-300 text-steel-300 rounded hover:text-indigo-500 hover:border-indigo-500">
           <i className="fal fa-sign-in mr-2"></i>Log In
         </Link>
