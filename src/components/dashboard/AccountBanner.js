3a1308aa785c73b5b8a201d97747e62fca12c7ab
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-16 15:52:38 +1000

    Changed text and added link in text

--- src/components/dashboard/AccountBanner.js
+++ src/components/dashboard/AccountBanner.js
@@ -3,24 +3,26 @@ import { Link } from "react-router-dom";
 
 export default () => {
   return (
-    <div className="bg-steel-700 mb-10 rounded p-5">
-      <h2 className="ml-2 pb-6">You are not logged in. Please login or sign up to start using Water Ledger dashboard.</h2>
-      <div className="text-right">
-        <Link to="/login">
-          <button
-            type="submit"
-            className="py-2 px-4 font-medium rounded-sm bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500"
-          >
-            <i className="fal fa-sign-in mr-2"></i>Log In
-          </button>
+    <div className="bg-steel-700 mb-8 rounded p-5 grid grid-cols-3">
+      <h2 className="ml-2 col-span-2">
+        You are not logged in. Please{" "}
+        <Link to="/login" className="text-steel-300 hover:text-indigo-300">
+          {" "}
+          login{" "}
         </Link>
-        <Link to="/signup">
-          <button
-            type="submit"
-            className="py-2 px-4 font-medium rounded-sm bg-steel-200 hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 ml-4"
-          >
-            <i className="fal fa-user-plus mr-2"></i>Sign Up
-          </button>
+        or
+        <Link to="/sign up" className="text-steel-300 hover:text-indigo-500">
+          {" "}
+          sign up{" "}
+        </Link>
+        to start using Water Ledger dashboard.
+      </h2>
+      <div className="text-right col-span-1">
+        <Link to="/login" className="no-underline text-right mr-3 p-2 px-8 mt-5 border-steel-300 text-steel-300 rounded">
+          <i className="fal fa-sign-in mr-2"></i>Log In
+        </Link>
+        <Link to="/signup" className="no-underline text-right mr-5 p-2 px-8 mt-5 border-steel-300 text-steel-300 rounded">
+          <i className="fal fa-user-plus mr-2"></i>Sign Up
         </Link>
       </div>
     </div>
