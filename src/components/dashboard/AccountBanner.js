1d1e89dac703f270489f33b92b9902d0b710267e
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-06-17 13:36:45 +1000

    Finalizing the <p> content

--- src/components/dashboard/AccountBanner.js
+++ src/components/dashboard/AccountBanner.js
@@ -4,19 +4,13 @@ import { Link } from "react-router-dom";
 export default () => {
   return (
     <div className="bg-steel-700 mb-8 rounded p-5 grid grid-cols-3">
-      <h2 className="ml-5 col-span-2">
+      <p className="ml-5 col-span-2">
         You are not logged in. Please
-        <Link to="/login" className="text-gray-500 hover:text-gray-600">
-          {" "}
-          login{" "}
-        </Link>
+        <Link to="/login" className="text-gray-500 hover:text-gray-600"> login </Link>
         or
-        <Link to="/signup" className="text-gray-500 hover:text-gray-600">
-          {" "}
-          sign up{" "}
-        </Link>
+        <Link to="/signup" className="text-gray-500 hover:text-gray-600"> sign up </Link>
         to start using Water Ledger dashboard.
-      </h2>
+      </p>
       <div className="text-right">
         <Link to="/login" className="mr-3 p-2 px-8 border-steel-300 text-steel-300 rounded hover:text-indigo-500 hover:border-indigo-500">
           <i className="fal fa-sign-in mr-2"></i>Log In
