66844c95cef5a53ff14ee13304ce00c05cad5e9b
Matt Burgess <matt@smallbatch.io>
2021-09-06 11:15:05 +0700

    remove websocket

--- src/components/Watchers.js
+++ src/components/Watchers.js
@@ -1,20 +1,4 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import SocketService from "../services/SocketService";
-import { addNotification } from "../redux/actions/actions";
-
-const socketService = SocketService.getInstance();
-const socket = socketService.getSocket();
-
 const Watchers = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    socket.on("LicenceCompleted", () => {
-      dispatch(addNotification({ text: "Your licence has been approved and you can now trade" }));
-    });
-  }, [dispatch]);
-
   return "";
 };
 
