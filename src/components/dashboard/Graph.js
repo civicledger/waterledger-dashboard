2df2eeafca7449f462c5d222e7054d91b13e3332
Farhad-Rezwan <farhad.rezwan@gmail.com>
2021-09-16 14:18:07 +1000

    Minor change

--- src/components/dashboard/Graph.js
+++ src/components/dashboard/Graph.js
@@ -168,105 +168,103 @@ const data = [
 
 export default () => {
   return (
-    <>
-      <div className="pb-10 px-2" id="graph" style={{ height: "400px" }}>
-        <ResponsiveLine
-          data={data}
-          theme={{
-            textColor: "#ffffff",
-            axis: {
-              domain: {
-                line: {
-                  stroke: "#ffffff",
-                  strokeWidth: 1,
-                },
-              },
-              ticks: {
-                line: {
-                  stroke: "#ffffff",
-                  strokeWidth: 1,
-                },
-              },
-              legend: {
-                text: {
-                  fontSize: 18,
-                },
-              },
-            },
-            grid: {
+    <div className="pb-10 px-2" id="graph" style={{ height: "400px" }}>
+      <ResponsiveLine
+        data={data}
+        theme={{
+          textColor: "#ffffff",
+          axis: {
+            domain: {
               line: {
-                stroke: "#2A3140",
-                strokeWidth: 0.5,
+                stroke: "#ffffff",
+                strokeWidth: 1,
               },
             },
-          }}
-          margin={{ top: 50, right: 110, bottom: 70, left: 60 }}
-          xScale={{ type: "point" }}
-          yScale={{ type: "linear", min: "auto", max: "auto", stacked: false, reverse: false }}
-          yFormat=" >-.2f"
-          axisTop={null}
-          axisBottom={{
-            orient: "bottom",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 45,
-            legend: "Month",
-            legendOffset: 60,
-            legendPosition: "middle",
-          }}
-          axisLeft={{
-            orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Price in $/ML",
-            legendOffset: -50,
-            legendPosition: "middle",
-          }}
-          axisRight={{
-            orient: "right",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "Price in $/ML",
-            legendOffset: 50,
-            legendPosition: "middle",
-          }}
-          pointSize={3}
-          isInteractive={false}
-          pointColor={{ theme: "background" }}
-          pointBorderWidth={2}
-          pointBorderColor={{ from: "serieColor" }}
-          pointLabelYOffset={-12}
-          useMesh={true}
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 115,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: "left-to-right",
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: "circle",
-              symbolBorderColor: "rgba(0, 0, 0, .5)",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemBackground: "rgba(12, 0, 0, .03)",
-                    itemOpacity: 1,
-                  },
-                },
-              ],
+            ticks: {
+              line: {
+                stroke: "#ffffff",
+                strokeWidth: 1,
+              },
             },
-          ]}
-        />
-      </div>
-    </>
+            legend: {
+              text: {
+                fontSize: 18,
+              },
+            },
+          },
+          grid: {
+            line: {
+              stroke: "#2A3140",
+              strokeWidth: 0.5,
+            },
+          },
+        }}
+        margin={{ top: 50, right: 110, bottom: 70, left: 60 }}
+        xScale={{ type: "point" }}
+        yScale={{ type: "linear", min: "auto", max: "auto", stacked: false, reverse: false }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 45,
+          legend: "Month",
+          legendOffset: 60,
+          legendPosition: "middle",
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Price in $/ML",
+          legendOffset: -50,
+          legendPosition: "middle",
+        }}
+        axisRight={{
+          orient: "right",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Price in $/ML",
+          legendOffset: 50,
+          legendPosition: "middle",
+        }}
+        pointSize={3}
+        isInteractive={false}
+        pointColor={{ theme: "background" }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "serieColor" }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 115,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(12, 0, 0, .03)",
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
   );
 };
