28fd576846faf8483204878d7925d044c8bd66b8
Farhad-Rezwan <farhad.rezwan@gmail.com>
2021-09-16 14:12:04 +1000

    Implement Nivo graph

--- src/components/dashboard/Graph.js
+++ src/components/dashboard/Graph.js
@@ -1,53 +1,272 @@
 import React from "react";
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, ChartLabel } from "react-vis";
-import "react-vis/dist/style.css";
+import { ResponsiveLine } from "@nivo/line";
 
-const dataFull = [
-  { x: new Date(2019, 7, 15), avg: 101, min: 100, max: 150 },
-  { x: new Date(2019, 8, 15), avg: 110, min: 90, max: 115 },
-  { x: new Date(2019, 9, 15), avg: 100, min: 93, max: 120 },
-  { x: new Date(2019, 10, 15), avg: 141, min: 79, max: 150 },
-  { x: new Date(2019, 11, 15), avg: 150, min: 133, max: 170 },
-  { x: new Date(2020, 1, 15), avg: 158, min: 145, max: 168 },
-  { x: new Date(2020, 2, 15), avg: 160, min: 148, max: 167 },
-  { x: new Date(2020, 3, 15), avg: 155, min: 150, max: 159 },
-  { x: new Date(2020, 4, 15), avg: 150, min: 141, max: 157 },
-  { x: new Date(2020, 5, 15), avg: 160, min: 142, max: 172 },
-  { x: new Date(2020, 6, 15), avg: 160, min: 150, max: 164 },
-  { x: new Date(2020, 7, 15), avg: 170, min: 130, max: 183 },
+const data = [
+  {
+    id: "avg",
+    color: "hsl(189, 70%, 50%)",
+    data: [
+      {
+        x: "September",
+        y: 101,
+      },
+      {
+        x: "October",
+        y: 110,
+      },
+      {
+        x: "November",
+        y: 100,
+      },
+      {
+        x: "December",
+        y: 141,
+      },
+      {
+        x: "January",
+        y: 150,
+      },
+      {
+        x: "February",
+        y: 158,
+      },
+      {
+        x: "March",
+        y: 160,
+      },
+      {
+        x: "April",
+        y: 155,
+      },
+      {
+        x: "May",
+        y: 150,
+      },
+      {
+        x: "June",
+        y: 160,
+      },
+      {
+        x: "July",
+        y: 160,
+      },
+      {
+        x: "August",
+        y: 170,
+      },
+    ],
+  },
+  {
+    id: "min",
+    color: "hsl(53, 70%, 50%)",
+    data: [
+      {
+        x: "September",
+        y: 100,
+      },
+      {
+        x: "October",
+        y: 90,
+      },
+      {
+        x: "November",
+        y: 93,
+      },
+      {
+        x: "December",
+        y: 79,
+      },
+      {
+        x: "January",
+        y: 133,
+      },
+      {
+        x: "February",
+        y: 145,
+      },
+      {
+        x: "March",
+        y: 148,
+      },
+      {
+        x: "April",
+        y: 150,
+      },
+      {
+        x: "May",
+        y: 141,
+      },
+      {
+        x: "June",
+        y: 142,
+      },
+      {
+        x: "July",
+        y: 150,
+      },
+      {
+        x: "August",
+        y: 130,
+      },
+    ],
+  },
+  {
+    id: "max",
+    color: "hsl(145, 70%, 50%)",
+    data: [
+      {
+        x: "September",
+        y: 150,
+      },
+      {
+        x: "October",
+        y: 115,
+      },
+      {
+        x: "November",
+        y: 120,
+      },
+      {
+        x: "December",
+        y: 150,
+      },
+      {
+        x: "January",
+        y: 170,
+      },
+      {
+        x: "February",
+        y: 168,
+      },
+      {
+        x: "March",
+        y: 167,
+      },
+      {
+        x: "April",
+        y: 159,
+      },
+      {
+        x: "May",
+        y: 157,
+      },
+      {
+        x: "June",
+        y: 172,
+      },
+      {
+        x: "July",
+        y: 164,
+      },
+      {
+        x: "August",
+        y: 183,
+      },
+    ],
+  },
 ];
 
 export default () => {
   return (
-    <div className="mr-10 p-3 hidden lg:block" id="graph">
-      <h2 className="ml-2 pb-6 text-2xl">Historical Prices</h2>
-      <XYPlot height={500} width={1000} xType="time" yDomain={[0, 200]}>
-        <HorizontalGridLines style={{ opacity: 0.05 }} />
-        <XAxis
-          style={{
-            text: { fill: "white" },
-            title: { fill: "white", fontWeight: 1000, fontSize: 16 },
+    <>
+      <div className="pb-10 px-2" id="graph" style={{ height: "400px" }}>
+        <ResponsiveLine
+          data={data}
+          theme={{
+            textColor: "#ffffff",
+            axis: {
+              domain: {
+                line: {
+                  stroke: "#ffffff",
+                  strokeWidth: 1,
+                },
+              },
+              ticks: {
+                line: {
+                  stroke: "#ffffff",
+                  strokeWidth: 1,
+                },
+              },
+              legend: {
+                text: {
+                  fontSize: 18,
+                },
+              },
+            },
+            grid: {
+              line: {
+                stroke: "#2A3140",
+                strokeWidth: 0.5,
+              },
+            },
           }}
-        />
-        <YAxis
-          style={{
-            text: { fill: "white" },
-            title: { fill: "white", fontWeight: 1000, fontSize: 16 },
+          margin={{ top: 50, right: 110, bottom: 70, left: 60 }}
+          xScale={{ type: "point" }}
+          yScale={{ type: "linear", min: "auto", max: "auto", stacked: false, reverse: false }}
+          yFormat=" >-.2f"
+          axisTop={null}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 45,
+            legend: "Month",
+            legendOffset: 60,
+            legendPosition: "middle",
           }}
+          axisLeft={{
+            orient: "left",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Price in $/ML",
+            legendOffset: -50,
+            legendPosition: "middle",
+          }}
+          axisRight={{
+            orient: "right",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Price in $/ML",
+            legendOffset: 50,
+            legendPosition: "middle",
+          }}
+          pointSize={3}
+          isInteractive={false}
+          pointColor={{ theme: "background" }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: "serieColor" }}
+          pointLabelYOffset={-12}
+          useMesh={true}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 115,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: "left-to-right",
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: "circle",
+              symbolBorderColor: "rgba(0, 0, 0, .5)",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemBackground: "rgba(12, 0, 0, .03)",
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
         />
-        <ChartLabel text="Month" className="alt-x-label" includeMargin={false} xPercent={0.94} yPercent={0.99} />
-        <ChartLabel
-          text="Price in $/ML"
-          className="alt-y-label"
-          includeMargin={false}
-          xPercent={0.03}
-          yPercent={0.26}
-          style={{ transform: "rotate(-90)" }}
-        />
-        <LineSeries data={dataFull} getY={d => d.avg} color="white" />
-        <LineSeries data={dataFull} getY={d => d.min} color="red" opacity={1} />
-        <LineSeries data={dataFull} getY={d => d.max} color="green" opacity={1} />
-      </XYPlot>
-    </div>
+      </div>
+    </>
   );
 };
