473e69c2475965b2c3e0724e8f870bfa20970bce
Matt Burgess <matt@smallbatch.io>
2021-09-27 17:36:09 +0700

    Improve ui for audit system

--- src/components/signup/AccountRow.js
+++ src/components/signup/AccountRow.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { formatKilolitres } from "../../utils/format";
 
 const AccountRow = ({ account, zone }) => {
   return (
     <>
       <div>{account.waterAccount}</div>
       <div>{zone}</div>
-      <div>{account.allocation}</div>
+      <div>{formatKilolitres(account.allocation)}</div>
     </>
   );
 };
