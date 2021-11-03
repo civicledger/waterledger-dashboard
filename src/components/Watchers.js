4541684b020c192b3617973a35009ca2e9cd0a4f
Matt Burgess <matt@smallbatch.io>
2021-11-04 01:17:13 +0700

    Update watchers

--- src/components/Watchers.js
+++ src/components/Watchers.js
@@ -1,44 +1,50 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 
-// import SocketService from "../services/SocketService";
-// import { addNotification } from "../redux/actions/actions";
-// import { queryClient } from "./queries";
+import SocketService from "../services/SocketService";
+import { addNotification } from "../redux/actions/actions";
+import { queryClient } from "./queries";
 
-// const socketService = SocketService.getInstance();
-// const socket = socketService.getSocket();
+const socketService = SocketService.getInstance();
+const socket = socketService.getSocket();
 
 const Watchers = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // socket.on("LicenceCompleted", () => {
-    //   dispatch(addNotification({ text: "Your licence has been approved and you can now trade" }));
-    // });
-    // socket.on("OrderAdded", async () => {
-    //   await queryClient.invalidateQueries("buyOrders");
-    //   await queryClient.invalidateQueries("sellOrders");
-    //   dispatch(addNotification({ text: "Refreshing order lists" }));
-    // });
-    // socket.on("TradeAdded", async () => {
-    //   await queryClient.invalidateQueries("trades");
-    //   dispatch(addNotification({ text: "Refreshing trades list" }));
-    // });
-    // socket.on("BalanceUpdated", async () => {
-    //   await queryClient.invalidateQueries("getLicence");
-    //   console.log("Should update balance");
-    //   dispatch(addNotification({ text: "Water Balance Updated" }));
-    // });
-    // socket.on("OrderTransactionMined", async () => {
-    //   dispatch(addNotification({ text: "Your order has been mined on the blockchain", type: "success" }));
-    // });
-    // socket.on("AcceptTransactionMined", async () => {
-    //   dispatch(addNotification({ text: "Your trade transaction has been mined on the blockchain", type: "success" }));
-    // });
+    socket.on("LicenceCompleted", () => {
+      dispatch(addNotification({ text: "Your licence has been approved and you can now trade" }));
+    });
+    socket.on("OrderAdded", async () => {
+      dispatch(addNotification({ text: "Your order has been mined on the blockchain", type: "success" }));
+    });
+
+    socket.on("OrdersModified", async () => {
+      await queryClient.invalidateQueries("buyOrders");
+      await queryClient.invalidateQueries("sellOrders");
+    });
+    socket.on("TradeAdded", async () => {
+      await queryClient.invalidateQueries("trades");
+      await queryClient.invalidateQueries("getScheme");
+      dispatch(addNotification({ text: "Refreshing trades list" }));
+    });
+    socket.on("BalanceUpdated", async () => {
+      await queryClient.invalidateQueries("getLicence");
+      console.log("Should update balance");
+      dispatch(addNotification({ text: "Water Balance Updated" }));
+    });
+    socket.on("BalancesUpdated", async () => {
+      await queryClient.invalidateQueries("getLicence");
+      console.log("Should update balance");
+      dispatch(addNotification({ text: "Water Balance Updated" }));
+    });
+    socket.on("AcceptTransactionMined", async () => {
+      dispatch(addNotification({ text: "Your trade transaction has been mined on the blockchain", type: "success" }));
+    });
   }, [dispatch]);
 
-  return "";
+  return <></>;
 };
 
 export default Watchers;
