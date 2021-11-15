2c81236010653d4dbd2a5ee51cea82fd29483ca6
Farhad-Rezwan <farhad.rezwan@gmail.com>
2021-11-15 16:01:43 +1100

    Initial context and provider for terminology

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import { useSelector } from "react-redux";
-import { userReducer, UserContext } from "./contexts";
+import { userReducer, UserContext, TerminologyContext, termReducer, initialTerminologyState } from "./contexts";
 import { Route, Switch } from "react-router-dom";
 import Watchers from "./Watchers";
 import Sidebar from "./app/Sidebar";
@@ -20,35 +20,42 @@ import TopNav from "./app/TopNav";
 
 import { QueryClientProvider } from "react-query";
 import { getScheme, queryClient } from "./queries";
+import Translate from "./common/Translate";
 
 export default props => {
   const [login, loginDispatch] = useReducer(userReducer, UserService.getLoggedInUser());
+  const terminologyStore = useReducer(termReducer, initialTerminologyState);
+
   queryClient.prefetchQuery("getScheme", getScheme);
+  const terminologyJson = { default: { scheme: "Scheme" }, COLORADO: { scheme: "Catchment" }, MDWSS: { scheme: "Scheme" } };
 
   const notifications = useSelector(state => state.notifications);
   return (
     <QueryClientProvider client={queryClient}>
-      <UserContext.Provider value={{ login, loginDispatch }}>
-        <Watchers />
-        <div className="flex min-h-screen bg-steel-900 text-steel-100">
-          <Sidebar />
-          <Notifications notifications={notifications} />
-          <div className="flex-1 flex flex-col">
-            <TopNav />
-            <Switch>
-              <Route path="/" exact component={Dashboard} />
-              <Route path="/licence" component={Licence} />
-              <Route path="/history" component={History} />
-              <Route path="/liabilities" component={Liabilities} />
-              <Route path="/audit" component={Audit} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/logout" component={Logout} />
-            </Switch>
+      <TerminologyContext.Provider value={{ terminologyStore, terminologyJson }}>
+        <UserContext.Provider value={{ login, loginDispatch }}>
+          <Watchers />
+          <div className="flex min-h-screen bg-steel-900 text-steel-100">
+            <Sidebar />
+            <Translate token="scheme" />
+            <Notifications notifications={notifications} />
+            <div className="flex-1 flex flex-col">
+              <TopNav />
+              <Switch>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/licence" component={Licence} />
+                <Route path="/history" component={History} />
+                <Route path="/liabilities" component={Liabilities} />
+                <Route path="/audit" component={Audit} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/logout" component={Logout} />
+              </Switch>
+            </div>
+            <Modals />
           </div>
-          <Modals />
-        </div>
-      </UserContext.Provider>
+        </UserContext.Provider>
+      </TerminologyContext.Provider>
     </QueryClientProvider>
   );
 };
