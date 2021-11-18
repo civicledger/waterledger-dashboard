a39bc9d82a1dd4a32fb7752f4a3ff36ad7bffee0
Farhad-Rezwan <farhad.rezwan@gmail.com>
2021-11-18 19:44:52 +1100

    Minor change

--- src/components/history/Trade.js
+++ src/components/history/Trade.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { formatAmount, formatKilolitres, formatShortDateObject } from "../../utils/format";
+import { queryClient } from "../queries";
 
 const Trade = ({ trade }) => {
+  const { terminologies } = queryClient.getQueryData("getTerminologies");
   return (
     <tr>
       <td className="p-2 pb-1">{formatAmount(trade.price)}</td>
-      <td className="p-2 pb-1">{formatKilolitres(trade.quantity)}</td>
+      <td className="p-2 pb-1">{formatKilolitres(trade.quantity, terminologies["unit"])}</td>
       <td className="p-2 pb-1">{trade.sellerAccount.zone.name}</td>
       <td className="p-2 pb-1">{trade.buyerAccount.zone.name}</td>
       <td className="p-2 pb-1 text-steel-300">{formatShortDateObject(trade.createdAt)}</td>
