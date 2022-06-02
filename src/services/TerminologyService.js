6ef8515166806a75108cfa30626e873f7f88503b
Farhad-Rezwan <farhad.rezwan@gmail.com>
2022-06-02 12:41:09 +1000

    Revert coloradobasin test and unused method

--- src/services/TerminologyService.js
+++ src/services/TerminologyService.js
@@ -6,14 +6,6 @@ axios.defaults.baseURL = baseURL;
 export default class TerminologyService extends BaseService {
   entity = "terminologies";
 
-  static getSavedTerminologies() {
-    const savedTerminologies = localStorage.getItem("terminologyObject");
-
-    if (!savedTerminologies) return axios.get(`${this.entity}`, { params: { schemeIdentifier: "default" } });
-
-    return JSON.parse(localStorage.getItem("terminologyObject")).terminologies;
-  }
-
   getCurrentTerminology() {
     return axios.get(`${this.entity}`, { params: { schemeIdentifier: getInstanceIdentifier() } });
   }
